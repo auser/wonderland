72940d18a12bf33dd6ebe1ebc8862d69c909fd0b
Ari Lerner <arilerner@mac.com>
2009-07-14 18:50:37 -0700

    Moved to sammy.js framework

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,49 @@
-var app = $.sammy(function() { with(this) {	
-	
-	debug = true;
-	element_selector = '#content';
-  
-  get('#/', function() { with(this) {
-    $('#content').text('Welcome!');
+;(function($) {
+  var app = $.sammy(function() { with(this) {
+    element_selector = '#content';
+    
+		var context = this;
+    var loaded = false;
+		// helpers
+	  var error = function(text) {context.trigger('error', {message: text});}
+		var notice = function(text) {context.trigger('notice', {message: text});}
+    
+    // display tasks
+    get('#/', function() { with (this) {
+			get_page("/control/status", {
+				success: function(data) {partial('/assets/templates/node_status.html', $(data.status)[0]);},
+				error: function() {error("Unable to load vhost information");}
+			});
+    }});
+
+    get('#/vhosts', function() { with (this) {			
+			get_page("/vhosts", {
+				success: function(data) {partial('/assets/templates/vhosts.html', data);},
+				error: function() {error("Unable to load vhosts");}
+			});
+    }});
+
+    get('#/users', function() { with (this) {			
+			get_page("/users", {
+				success: function(data) {partial('/assets/templates/users.html', data);},
+				error: function() {error("Unable to load users");}
+			});
+    }});
+
+    get('#/rabbit_status', function() { with (this) {
+			redirect("#/");
+			return false;
+    }});
+    
+    bind('loaded', function() { with(this) { redirect('#/'); }});
+    
+    bind('error', function(e, data) { with(this) {show_div(data, "#error");}});
+		bind('notice', function(e, data) { with(this) {show_div(data, "#notice");}});
+    
+    
   }});
-
-}});
-
-$(function() {
-  app.run();
-});
+  
+  $(function() {
+    app.run('#/');
+  })
+})(jQuery);
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,46 @@
-function originalBindings(state) {
-	
-	$(state.currentBinding).each(function(i, binding) {
-		binding();
-	});
-	state.currentBinding = [];
-	return state;
+// helpers
+function show_div(data, div_id) {
+	var div = $(div_id); 
+	div.html(data.message);
+	div.animate({height: "30px"}, 300);
+	setTimeout(function(){ div.animate({height: "0px"}, 300, function(){ div.html(""); div.hide()})}, 1400);
+}
+
+function refresh_page(location) {
+	get_page(location, {
+		success: function(data) {
+			console.log("Get data");
+			$(document.body).html(data)
+		}
+	})
+}
+
+// AJAX
+var aliceUrl = "http://localhost:9999";
+
+function get_page(url, opts) {
+	_ajax_method("GET", url, opts);
+};
+
+function post(url, opts) {
+	_ajax_method("POST", url, opts);
+};
+
+function post_delete(url, opts) {
+	_ajax_method("DELETE", url, opts);
+};
+
+function _ajax_method(meth, url, opts) {
+	$.ajax({
+		url: aliceUrl + url,
+		type: meth,
+		cache: false,
+		data: opts.data,
+		dataType: "json",
+		success: opts.success,
+		error: opts.error
+	 });
+	return false;
 };
 
 function traverse(key, jsonObj, func) {
